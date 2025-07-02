94de9a1e68d6308ccf4066a5967dde43c8db9826
Bibek Hanal <bbekhamal26@gmail.com>
2025-07-02 14:28:07 +1000

    mongo commit

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 import express from 'express';
+import { dbConnect } from './src/config/dbConfig.js';
+import dotenv from 'dotenv';
 
 const app = express ()
+
 const PORT = 3000;
 
+// db connection mongo
+app.use(express.json());
+
+dotenv.config();
+dbConnect();
+
+// simple server
 app.get('/', (req, res) => {
-    res.send("The server is live")
+    res.send("The server is live and mongo is working")
 });
 
 app.listen(PORT, ()=> {
